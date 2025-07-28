52f36643e95442cbc2fbaea7191ecab1c5407fcd
Dev-Devanshu <writeds.dev@gmail.com>
2025-07-28 13:03:26 +0530

    project done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-primary py-12">
       <div className="container mx-auto">
         <p className="text-white text-center">
-          Copyright &copy; Nigar Safarova 🤍 2022. All rights reserved.
+          Copyright &copy; dev 🤍 2025. All rights reserved.
         </p>
       </div>
     </footer>
